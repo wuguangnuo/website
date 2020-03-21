2db90af607f49e6b1edd13a30155409833466e98
吴广诺 <wuguangnuo@qq.com>
2020-03-22 00:36:15 +0800

    VistorChart

--- src/main/java/cn/wgn/website/service/impl/CacheServiceImpl.java
+++ src/main/java/cn/wgn/website/service/impl/CacheServiceImpl.java
@@ -1,32 +1,35 @@
 package cn.wgn.website.service.impl;
 
 import cn.wgn.website.entity.CallEntity;
+import cn.wgn.website.entity.Dictionary;
+import cn.wgn.website.entity.VistorEntity;
 import cn.wgn.website.mapper.CallMapper;
+import cn.wgn.website.mapper.DictionaryMapper;
+import cn.wgn.website.mapper.VistorMapper;
 import cn.wgn.website.service.ICacheService;
 import cn.wgn.website.utils.DateUtil;
 import cn.wgn.website.utils.FileUtil;
 import com.alibaba.fastjson.JSON;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.github.abel533.echarts.AxisPointer;
-import com.github.abel533.echarts.Grid;
-import com.github.abel533.echarts.Legend;
-import com.github.abel533.echarts.Option;
+import com.github.abel533.echarts.*;
 import com.github.abel533.echarts.axis.CategoryAxis;
 import com.github.abel533.echarts.axis.ValueAxis;
 import com.github.abel533.echarts.code.*;
 import com.github.abel533.echarts.feature.MagicType;
 import com.github.abel533.echarts.series.Bar;
+import com.github.abel533.echarts.series.Line;
 import com.github.abel533.echarts.style.ItemStyle;
 import com.github.abel533.echarts.style.itemstyle.Normal;
+import com.google.common.base.Strings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
-import java.util.LinkedHashMap;
-import java.util.List;
+import java.util.*;
 
 /**
  * Redis 缓存服务
@@ -38,6 +41,10 @@ import java.util.List;
 public class CacheServiceImpl implements ICacheService {
     @Autowired
     private CallMapper callMapper;
+    @Autowired
+    private VistorMapper vistorMapper;
+    @Autowired
+    private DictionaryMapper dictionaryMapper;
     @Autowired
     private FileUtil fileUtil;
 
@@ -68,13 +75,14 @@ public class CacheServiceImpl implements ICacheService {
     public String getHomeChart() {
         List<CallEntity> callEntityList = callMapper.selectList(
                 new QueryWrapper<CallEntity>().lambda()
+                        .select(CallEntity::getLk, CallEntity::getTm)
                         .between(CallEntity::getTm, LocalDateTime.of(LocalDate.now(), LocalTime.MIN).minusDays(6), LocalDateTime.now())
         );
 
         // 日期坐标轴
         String[] yAxis = new String[7];
-        for (int i = 0; i < 7; i++) {
-            yAxis[i] = LocalDate.now().minusDays(6 - i).toString();
+        for (int i = 0; i < yAxis.length; i++) {
+            yAxis[i] = LocalDate.now().minusDays(yAxis.length - 1 - i).toString();
         }
 
         // LinkedHashMap<接口名称，LinkedHashMap<日期，数量>>
@@ -124,9 +132,102 @@ public class CacheServiceImpl implements ICacheService {
      * @return
      */
     @Override
-    @Cacheable(value = "VistorChart", key = "")
+    @Cacheable(value = "VistorChart", key = "#dateTime1.toLocalDate()+'_'+#dateTime2.toLocalDate()")
     public String getVistorChart(LocalDateTime dateTime1, LocalDateTime dateTime2) {
-        return null;
+        List<VistorEntity> vistorEntityList = vistorMapper.selectList(
+                new QueryWrapper<VistorEntity>().lambda()
+                        .select(VistorEntity::getAg, VistorEntity::getTm)
+                        .between(VistorEntity::getTm, dateTime1, dateTime2)
+        );
+
+        // 日期坐标轴
+        String[] yAxis = new String[(int) Duration.between(dateTime1, dateTime2).toDays() + 1];
+        for (int i = 0; i < yAxis.length; i++) {
+            yAxis[i] = LocalDate.now().minusDays(yAxis.length - 1 - i).toString();
+        }
+
+        // 所有折线类型
+        List<Dictionary> dictionaryList = dictionaryMapper.selectList(
+                new QueryWrapper<Dictionary>().lambda()
+                        .eq(Dictionary::getGroupKey, "vistor_show")
+        );
+//        List<String> vistorShow = dictionaryList.stream().map(
+//                Dictionary::getCodeNote
+//        ).collect(Collectors.toList());
+        // all -> 全部
+        Map<String, String> vistorShow = new HashMap<>();
+        for (Dictionary d : dictionaryList) {
+//            改表后删除这三个条件
+            if (d.getCodeValue().equals("all") || d.getCodeValue().equals("vistor") || d.getCodeValue().equals("others")) {
+                continue;
+            }
+            vistorShow.put(d.getCodeValue(), d.getCodeNote());
+        }
+
+        // LinkedHashMap<接口名称，LinkedHashMap<日期，数量>>
+        LinkedHashMap<String, LinkedHashMap<String, Integer>> data = new LinkedHashMap<>();
+
+        LinkedHashMap<String, Integer> zeroLine = new LinkedHashMap<>();
+        for (String d : yAxis) {
+            zeroLine.put(d, 0);
+        }
+
+        // 初始化接口名称
+        data.put("总访问量", (LinkedHashMap<String, Integer>) zeroLine.clone());
+        data.put("访客", (LinkedHashMap<String, Integer>) zeroLine.clone());
+        for (String key : vistorShow.keySet()) {
+            data.put(vistorShow.get(key), (LinkedHashMap<String, Integer>) zeroLine.clone());
+        }
+        data.put("其他流量", (LinkedHashMap<String, Integer>) zeroLine.clone());
+
+        for (VistorEntity e : vistorEntityList) {
+            String date = e.getTm().toLocalDate().toString();
+            String ag = e.getAg();
+            data.get("总访问量").put(date, data.get("总访问量").get(date) + 1);
+            if (Strings.isNullOrEmpty(ag)) {
+                continue;
+            }
+
+            // 正则检测蜘蛛
+            if (ag.contains("spider") || ag.contains("bot")) {
+                // 匹配字典中的蜘蛛类型
+                boolean isNormalSpider = false;
+                for (String key : vistorShow.keySet()) {
+                    if (ag.contains(key)) {
+                        int num = data.get(vistorShow.get(key)).get(date) + 1;
+                        data.get(vistorShow.get(key)).put(date, num);
+                        isNormalSpider = true;
+                        break;
+                    }
+                }
+                if (!isNormalSpider) {
+                    data.get("其他流量").put(date, data.get("其他流量").get(date) + 1);
+                }
+            } else {
+                data.get("访客").put(date, data.get("访客").get(date) + 1);
+            }
+        }
+
+        Option option = new Option();
+        option.title().text("访客类型统计图").subtext("api.wuguangnuo.cn 数据更新时间:" + DateUtil.dateFormat(null, DateUtil.MINUTE_PATTERN)).left("3%");
+        option.tooltip().trigger(Trigger.axis);
+        option.legend(new Legend().data(data.keySet().toArray()));
+        // yaxisindex none
+        option.toolbox().show(true).right("3%").feature(new DataZoom().yAxisIndex("none"), Tool.restore, Tool.dataView, Tool.saveAsImage);
+
+        List<DataZoom> dataZoomList = new ArrayList<>();
+        dataZoomList.add(new DataZoom().startValue(LocalDate.now().minusMonths(1)));
+        dataZoomList.add(new DataZoom().type(DataZoomType.inside));
+        option.setDataZoom(dataZoomList);
+        option.xAxis(new CategoryAxis().data(yAxis).boundaryGap(false).type(AxisType.category));
+        option.yAxis(new ValueAxis().type(AxisType.value));
+        for (String key : data.keySet()) {
+            option.series(new Line(key)
+                    .data(data.get(key).values().toArray())
+                    .name(key));
+        }
+
+        return JSON.toJSONString(option);
     }
 
     /**
