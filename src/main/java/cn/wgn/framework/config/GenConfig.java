0079b6ca52f6ed34b239df7b9a41b85f8a1f0368
wuguangnuo <wuguangnuo@qq.com>
2020-08-09 15:28:45 +0800

    todu auto config

---
+++ src/main/java/cn/wgn/framework/config/GenConfig.java
@@ -0,0 +1,13 @@
+//package cn.wgn.framework.config;
+//
+//import org.springframework.boot.context.properties.ConfigurationProperties;
+//import org.springframework.stereotype.Component;
+//
+///**
+// * @author WuGuangNuo
+// * @date Created in 2020/8/9 14:47
+// */
+//@Component
+//@ConfigurationProperties(prefix = "private-config")
+//public class GenConfig {
+//}
