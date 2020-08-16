0f9fc372d7765a10b7b04fc619f2e059ad577275
wuguangnuo <wuguangnuo@qq.com>
2020-08-16 11:44:35 +0800

    添加宝塔API接口

--- src/main/java/cn/wgn/website/sys/task/PatrolTask.java
+++ src/main/java/cn/wgn/website/sys/task/PatrolTask.java
@@ -1,23 +1,27 @@
 package cn.wgn.website.sys.task;
 
 import cn.wgn.framework.utils.DateUtil;
+import cn.wgn.framework.utils.EncryptUtil;
 import cn.wgn.framework.utils.HtmlModel;
+import cn.wgn.framework.utils.HttpUtil;
 import cn.wgn.framework.utils.job.JobConstants;
 import cn.wgn.framework.utils.mail.EmailInfo;
 import cn.wgn.framework.utils.mail.EmailUtil;
 import cn.wgn.framework.web.entity.JobLogEntity;
 import cn.wgn.framework.web.service.IJobLogService;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.sun.management.OperatingSystemMXBean;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.lang.management.ManagementFactory;
 import java.text.DecimalFormat;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -41,6 +45,15 @@ public class PatrolTask {
      * 最近发送邮件时间戳
      */
     private static long lastEmail = 0;
+    /**
+     * 宝塔API接口密钥
+     */
+    @Value("${private-config.bt.btSign}")
+    private String btSign;
+    /**
+     * 获取系统基础统计API
+     */
+    private static final String SYSTEM_TOTAL = "http://bt.wuguangnuo.cn:8888/system?action=GetSystemTotal";
     @Autowired
     private IJobLogService jobLogService;
 
@@ -49,10 +62,14 @@ public class PatrolTask {
      */
     public void doctor() {
         count++;
-        OperatingSystemMXBean osmxb = (OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
-        long totalPhysicalMemorySize = osmxb.getTotalPhysicalMemorySize();
-        long freePhysicalMemorySize = osmxb.getFreePhysicalMemorySize();
-        Double memoryPercent = (1 - (double) freePhysicalMemorySize / (double) totalPhysicalMemorySize) * 100D;
+        // 获取系统状态信息
+        String sysJson = getSystemTotal();
+        JSONObject object = JSON.parseObject(sysJson);
+        boolean isSysSuc = Boolean.TRUE;
+        if (object.get("status") != null && !(Boolean) object.get("status")) {
+            // 获取系统信息失败
+            isSysSuc = false;
+        }
 
         // 定时任务数量状态
         LambdaQueryWrapper<JobLogEntity> qw = new QueryWrapper<JobLogEntity>().lambda().between(
@@ -74,22 +91,33 @@ public class PatrolTask {
         );
         long x = list.stream().map(JobLogEntity::getStatus).filter(JobConstants.FAIL::equals).count();
 
-        String report = "<p>======== REPORT ========" +
-                "</p><p>现在时间：" + DateUtil.dateFormat(null, DateUtil.HOUR_PATTERN) + "，系统已运行：" + DateUtil.diffDate(TM) +
-                "</p><p>内存使用率：" + new DecimalFormat("###.###").format(memoryPercent) + "%" +
-                "</p><p>巡警医生执行次数：" + new DecimalFormat("#,###").format(count) +
-                "</p><p>定时任务最近一天内执行次数：" + allNum + "，其中错误次数：" + errNum +
-                "</p><p>定时任务最近10次执行错误数：" + x +
-                "</p><p>======== END ========</p>";
+        StringBuilder sb = new StringBuilder();
+        DecimalFormat numberFormat = new DecimalFormat("#,###");
+        sb.append("<p>======== REPORT ========");
+        sb.append("</p><p>现在时间：" + DateUtil.dateFormat(null, DateUtil.HOUR_PATTERN) + "，系统已运行：" + DateUtil.diffDate(TM));
+        if (isSysSuc) {
+            double memRealUsed = Double.parseDouble(object.get("memRealUsed") + "");
+            double memTotal = Double.parseDouble(object.get("memTotal") + "");
+            double memoryPercent = (memRealUsed / memTotal) * 100D;
+            sb.append("</p><p>服务器信息：" + object.get("system") + "，已运行" + object.get("time"));
+            sb.append("</p><p>物理内存使用情况：" + memRealUsed + "/" + memTotal + "（" + new DecimalFormat("###.###").format(memoryPercent) + "%）");
+        } else {
+            sb.append("</p><p>获取系统信息失败：" + object.get("msg"));
+        }
+        sb.append("</p><p>巡警医生执行次数：" + numberFormat.format(count));
+        sb.append("</p><p>定时任务最近一天内执行次数：" + numberFormat.format(allNum) + "，其中错误次数：" + numberFormat.format(errNum));
+        sb.append("</p><p>定时任务最近10次执行错误数：" + x);
+        sb.append("</p><p>======== END ========</p>");
 
+        // 达到条件发送邮件
         if (x >= 3 && isEmail()) {
             lastEmail = System.currentTimeMillis();
             String subject = "巡警医生报告邮件 from wgn API";
-            String content = HtmlModel.mailBody("巡警医生报告邮件", report);
+            String content = HtmlModel.mailBody("巡警医生报告邮件", sb.toString());
             EmailInfo emailInfo = new EmailInfo("wuguangnuo@qq.com", subject, content);
             EmailUtil.sendHtmlMail(emailInfo);
         }
-        log.info(report.replaceAll("<p>", "\r\n").replaceAll("</p>", ""));
+        log.info(sb.toString().replaceAll("<p>", "\r\n").replaceAll("</p>", ""));
     }
 
     /**
@@ -106,5 +134,22 @@ public class PatrolTask {
         LocalTime start = LocalTime.of(0, 0);
         LocalTime end = LocalTime.of(8, 0);
         return !(now.isAfter(start) && now.isBefore(end));
+    }
+
+    /**
+     * 获取系统基础统计
+     *
+     * @return
+     */
+    private String getSystemTotal() {
+        String timestamp = String.valueOf(System.currentTimeMillis());
+        String md5Sign = EncryptUtil.getMD5Str(btSign);
+        String token = EncryptUtil.getMD5Str(timestamp + md5Sign);
+
+        HashMap<String, String> map = new HashMap<>();
+        map.put("request_time", timestamp);
+        map.put("request_token", token);
+
+        return HttpUtil.httpPostMap(map, SYSTEM_TOTAL);
     }
 }
