2edda16525a99bde7dcbc6cccafee94c77e6dd69
wuguangnuo <wuguangnuo@qq.com>
2021-06-25 00:34:32 +0800

    BaiduTask

--- src/main/java/cn/wgn/website/sys/task/BaiduTask.java
+++ src/main/java/cn/wgn/website/sys/task/BaiduTask.java
@@ -31,7 +31,7 @@ public class BaiduTask {
     /**
      * 定时爬取 百度实时热点排行榜
      */
-    private static final String BAIDU_TOP_URL = "http://top.baidu.com/buzz?b=1";
+    private static final String BAIDU_TOP_URL = "https://top.baidu.com/board?tab=realtime";
 
     @Autowired
     private IBaidutopService baidutopService;
@@ -43,37 +43,30 @@ public class BaiduTask {
     public void botBaiduTopTask() {
         try {
             Document document = Jsoup.parse(new URL(BAIDU_TOP_URL), 10000);
-            Elements elements = document.getElementsByClass("list-table")
-                    .get(0).getElementsByTag("tbody").get(0).getElementsByTag("tr");
+            Elements elements = document.getElementsByClass("category-wrap_iQLoo horizontal_1eKyQ");
 
             // 百度风云榜 无自带排名，此处加上排序
             int i = 0;
             LocalDateTime createTime = LocalDateTime.now();
             List<BaidutopEntity> list = new ArrayList<>();
             for (Element e : elements) {
-                if (e.getElementsByClass("first").size() > 0 && e.getElementsByClass("list-title").size() > 0) {
-                    System.out.println(e.toString());
-                    String ranking = e.getElementsByClass("first").get(0).getElementsByTag("span").get(0).html();
-                    String title = e.getElementsByClass("list-title").get(0).html();
-                    String score = e.getElementsByClass("last").get(0).getElementsByTag("span").get(0).html();
-                    String state = null;
-                    if (e.getElementsByClass("keyword").get(0).getElementsByClass("icon").size() > 0) {
-                        state = e.getElementsByClass("keyword").get(0).getElementsByClass("icon").get(0).attr("class")
-                                .replaceAll("icon ", "").replaceAll("icon-", "");
-                    }
-                    String trend = e.getElementsByClass("last").get(0).getElementsByTag("span").get(0).attr("class")
-                            .replaceAll("icon-", "");
+                System.out.println(e.toString());
+                String ranking = e.getElementsByClass("img-wrapper_29V76").get(0).getElementsByClass("index_1Ew5p").get(0).html();
+                String title = e.getElementsByClass("title_dIF3B").get(0).childNodes().get(0).toString();
+                String score = e.getElementsByClass("hot-index_1Bl1a").get(0).html();
+                String state = e.getElementsByClass("title_dIF3B").get(0).childNodes().get(1).childNodes().get(0).toString().trim();
+//                    String trend = e.getElementsByClass("last").get(0).getElementsByTag("span").get(0).attr("class")
+//                            .replaceAll("icon-", "");
 
-                    i++;
-                    BaidutopEntity entity = new BaidutopEntity();
-                    entity.setRanking(ranking);
-                    entity.setTitle(title);
-                    entity.setScore(score);
-                    entity.setState(state);
-                    entity.setTrend(trend);
-                    entity.setCreateTime(createTime);
-                    list.add(entity);
-                }
+                i++;
+                BaidutopEntity entity = new BaidutopEntity();
+                entity.setRanking(ranking);
+                entity.setTitle(title);
+                entity.setScore(score);
+                entity.setState(state);
+//                    entity.setTrend(trend);
+                entity.setCreateTime(createTime);
+                list.add(entity);
             }
 
             // 批量保存
