d86523f2843db771f3407c44f8e59b77eb5356f8
wuguangnuo <wuguangnuo@qq.com>
2021-09-24 16:50:14 +0800

    A站长度限制

--- src/main/java/cn/wgn/website/sys/task/AcfunTask.java
+++ src/main/java/cn/wgn/website/sys/task/AcfunTask.java
@@ -8,6 +8,7 @@ import cn.wgn.website.sys.service.IAcfunrkService;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -83,7 +84,7 @@ public class AcfunTask {
                 entity.setRanking(ranking);
                 entity.setDougaId(dougaId);
                 entity.setTitle(title);
-                entity.setContentDesc(contentDesc);
+                entity.setContentDesc(StringUtils.substring(contentDesc, 0, 1000)); // 数据库最大长度 1K
                 entity.setUserId(userId);
                 entity.setUserName(userName);
                 entity.setChannel(channel);
